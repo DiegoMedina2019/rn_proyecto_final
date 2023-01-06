9ecf951c33cba7eb9fbf163574f0332794b75b6f
Diego Medina <xecnon@gmail.com>
2023-01-06 10:14:51 -0300

    se termino vista Inicio y se agrego react navigation(falta terminar)

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Inicio from './views/Inicio/Inicio';
 import Login from './views/login/Login';
 import Register from './views/register/Register';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Register />
+      {/* <Inicio /> */}
+      {/* <Register /> */}
       {/* <Login /> */}
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Inicio" component={Inicio} />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
     </View>
   );
 }
---
+++ views/Inicio/Inicio.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Image, StyleSheet, Text, TextInput, Touchable, View } from 'react-native'
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+const Inicio = () => {
+  return (
+    <View >
+      <View style={estiloInicio.seccImg}>
+        <Image 
+          source={require('../../assets/images/elipse.png')}
+        />
+      </View>
+      <View style={estiloInicio.vista}>
+        <View>
+
+          <Image 
+            source={require('../../assets/images/onboarding.png')} 
+            style={{width: 200, height: 200,alignSelf:'center'}}
+          />
+          <Text style={{fontWeight:'bold',alignSelf:'center',fontSize:25,marginBottom:10,marginTop:25}}>
+            Get things done with TODO
+          </Text>
+          <Text style={{alignSelf:'center',fontSize:15,marginBottom:20,textAlign:'center'}}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aliquam odio quis cum alias. Delectus illo repellendus fuga odit fugiat praesentium atque similique iure nisi reiciendis consequuntur, nemo esse vitae.
+          </Text>
+
+        </View>
+
+        <View style={{marginTop:70}}>
+          
+
+          <Button text={"Get Started"}/>
+
+        </View>
+      </View>
+
+    </View>
+  )
+}
+
+const estiloInicio = new StyleSheet.create({
+  vista:{
+    width:"80%",
+    justifyContent:'center',
+    alignContent:'center',
+    alignSelf:'center'
+   // margin:30
+  },
+  seccImg:{
+    right:50,
+    marginLeft:-40,
+    marginTop:-90
+  }  
+});
+
+export default Inicio
